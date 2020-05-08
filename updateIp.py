22fe460ff0a5e981ef1599b8ed55aeb314db8b53
Alberto Zurini <alberto.zurini@gmail.com>
2020-05-08 13:10:47 +0200

    IP got via DNS request

--- updateIp.py
+++ updateIp.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, subprocess
 
 AUTH_EMAIL = 'YOUR_EMAIL@YOUR_DOMAIN.com'
 AUTH_KEY = 'YOUR_CLOUDFLARE_API_KEY'
@@ -6,7 +6,8 @@ DOMAIN = 'YOUR_DOMAIN.tk'
 
 sync_ip = [DOMAIN, 'www.'+DOMAIN]
 
-my_ip = requests.get('https://jsonip.com/').json()['ip']
+my_ip = subprocess.Popen("""dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'""", shell=True, stdout=subprocess.PIPE).stdout.read()
+my_ip = str(my_ip).split("'")[1].split('\\')[0]
 
 headers = {'X-Auth-Email': AUTH_EMAIL, 'X-Auth-Key': AUTH_KEY, 'Content-Type': 'application/json'}
 zone_req = requests.get('https://api.cloudflare.com/client/v4/zones', headers=headers).json()
